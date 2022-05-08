4b59552ab4ec25e9030e9a77feb81e868ffe33f7
hireoochen@gmail.com <hireoochen@gmail.com>
2022-05-08 23:04:59 +0800

    Update List.vue

--- todos/src/components/TodoView/List.vue
+++ todos/src/components/TodoView/List.vue
@@ -1,68 +1,136 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>
+  <main class="w-full">
     <div class="mb-5">
       <h3 class="text-lg">Todos</h3>
       <ul>
-        <li class="text-base tracking-wide leading-5 mb-1">
-          <input
-            id="default-checkbox1"
-            type="checkbox"
-            value=""
-            class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            for="default-checkbox1"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >Default checkbox</label
+        <li class="text-base tracking-wide leading-5 my-2 flex justify-between">
+          <div>
+            <input
+              id="default-checkbox1"
+              type="checkbox"
+              value=""
+              class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+              for="default-checkbox1"
+              class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >Default checkbox
+            </label>
+          </div>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 inline-block"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="1"
           >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
         </li>
-        <li class="text-base tracking-wide leading-5 mb-1">
-          <input
-            id="default-checkbox2"
-            type="checkbox"
-            value=""
-            class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            for="default-checkbox2"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >Default checkbox</label
+        <li class="text-base tracking-wide leading-5 my-2 flex justify-between">
+          <div>
+            <input
+              id="default-checkbox2"
+              type="checkbox"
+              value=""
+              class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+              for="default-checkbox2"
+              class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >Default checkbox
+            </label>
+          </div>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 inline-block"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="1"
           >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
         </li>
-        <li class="text-base tracking-wide leading-5 mb-1">
-          <input
-            id="default-checkbox3"
-            type="checkbox"
-            value=""
-            class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            for="default-checkbox3"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >Default checkbox</label
+        <li class="text-base tracking-wide leading-5 my-2 flex justify-between">
+          <div>
+            <input
+              id="default-checkbox3"
+              type="checkbox"
+              value=""
+              class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+              for="default-checkbox3"
+              class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >Default checkbox
+            </label>
+          </div>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 inline-block"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="1"
           >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
         </li>
-        <li class="text-base tracking-wide leading-5 mb-1">
-          <input
-            id="default-checkbox4"
-            type="checkbox"
-            value=""
-            class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            for="default-checkbox4"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >Default checkbox</label
+        <li class="text-base tracking-wide leading-5 my-2 flex justify-between">
+          <div>
+            <input
+              id="default-checkbox4"
+              type="checkbox"
+              value=""
+              class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+              for="default-checkbox4"
+              class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >Default checkbox
+            </label>
+          </div>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 inline-block"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="1"
           >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
         </li>
       </ul>
     </div>
     <div class="mb-5">
       <h3 class="text-lg">In progress</h3>
       <ul>
-        <li class="text-base tracking-wide leading-5 mb-1">
+        <li class="text-base tracking-wide leading-5 my-2">
           <input
             id="default-checkbox11"
             type="checkbox"
@@ -75,7 +143,7 @@
             >Default checkbox</label
           >
         </li>
-        <li class="text-base tracking-wide leading-5 mb-1">
+        <li class="text-base tracking-wide leading-5 my-2">
           <input
             id="default-checkbox22"
             type="checkbox"
@@ -88,7 +156,7 @@
             >Default checkbox</label
           >
         </li>
-        <li class="text-base tracking-wide leading-5 mb-1">
+        <li class="text-base tracking-wide leading-5 my-2">
           <input
             id="default-checkbox33"
             type="checkbox"
@@ -101,7 +169,7 @@
             >Default checkbox</label
           >
         </li>
-        <li class="text-base tracking-wide leading-5 mb-1">
+        <li class="text-base tracking-wide leading-5 my-2">
           <input
             id="default-checkbox44"
             type="checkbox"
@@ -119,7 +187,7 @@
     <div class="mb-5">
       <h3 class="text-lg">Done</h3>
       <ul>
-        <li class="text-base tracking-wide leading-5 mb-1">
+        <li class="text-base tracking-wide leading-5 my-2">
           <input
             id="default-checkbox111"
             type="checkbox"
@@ -128,11 +196,11 @@
           />
           <label
             for="default-checkbox111"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300 line-through"
             >Default checkbox</label
           >
         </li>
-        <li class="text-base tracking-wide leading-5 mb-1">
+        <li class="text-base tracking-wide leading-5 my-2">
           <input
             id="default-checkbox222"
             type="checkbox"
@@ -141,11 +209,11 @@
           />
           <label
             for="default-checkbox222"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300 line-through"
             >Default checkbox</label
           >
         </li>
-        <li class="text-base tracking-wide leading-5 mb-1">
+        <li class="text-base tracking-wide leading-5 my-2">
           <input
             id="default-checkbox333"
             type="checkbox"
@@ -154,11 +222,11 @@
           />
           <label
             for="default-checkbox333"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300 line-through"
             >Default checkbox</label
           >
         </li>
-        <li class="text-base tracking-wide leading-5 mb-1">
+        <li class="text-base tracking-wide leading-5 my-2">
           <input
             id="default-checkbox444"
             type="checkbox"
@@ -167,7 +235,7 @@
           />
           <label
             for="default-checkbox444"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300 line-through"
             >Default checkbox</label
           >
         </li>
