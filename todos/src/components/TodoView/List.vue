800f1ae6d5f3256daf964703d9030d765837c186
hireoochen@gmail.com <hireoochen@gmail.com>
2022-05-10 23:47:28 +0800

    update

--- todos/src/components/TodoView/List.vue
+++ todos/src/components/TodoView/List.vue
@@ -1,244 +1,97 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface ITodo {
+  id: number;
+  text: string;
+  status: "pending" | "working" | "done";
+}
+
+
+defineProps(["todos"]);
+</script>
 
 <template>
   <main class="w-full">
     <div class="mb-5">
       <h3 class="text-lg">Todos</h3>
       <ul>
-        <li class="text-base tracking-wide leading-5 my-2 flex justify-between">
-          <div>
-            <input
-              id="default-checkbox1"
-              type="checkbox"
-              value=""
-              class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-            />
-            <label
-              for="default-checkbox1"
-              class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >Default checkbox
-            </label>
-          </div>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 inline-block"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="1"
+        <template v-for="todo in todos">
+          <li
+            v-if="todo.status === 'pending'"
+            :key="todo.id"
+            class="text-base tracking-wide leading-5 my-2 flex justify-between"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-        </li>
-        <li class="text-base tracking-wide leading-5 my-2 flex justify-between">
-          <div>
-            <input
-              id="default-checkbox2"
-              type="checkbox"
-              value=""
-              class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-            />
-            <label
-              for="default-checkbox2"
-              class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >Default checkbox
-            </label>
-          </div>
+            <div>
+              <input
+                :id="todo.id"
+                type="checkbox"
+                value="todo.id"
+                class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                :for="todo.id"
+                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >{{ todo.text }}
+              </label>
+            </div>
 
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 inline-block"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="1"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-        </li>
-        <li class="text-base tracking-wide leading-5 my-2 flex justify-between">
-          <div>
-            <input
-              id="default-checkbox3"
-              type="checkbox"
-              value=""
-              class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-            />
-            <label
-              for="default-checkbox3"
-              class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >Default checkbox
-            </label>
-          </div>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 inline-block"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="1"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-        </li>
-        <li class="text-base tracking-wide leading-5 my-2 flex justify-between">
-          <div>
-            <input
-              id="default-checkbox4"
-              type="checkbox"
-              value=""
-              class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-            />
-            <label
-              for="default-checkbox4"
-              class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >Default checkbox
-            </label>
-          </div>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 inline-block"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="1"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-        </li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6 inline-block"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="1"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </li>
+        </template>
       </ul>
     </div>
     <div class="mb-5">
       <h3 class="text-lg">In progress</h3>
       <ul>
-        <li class="text-base tracking-wide leading-5 my-2">
-          <input
-            id="default-checkbox11"
-            type="checkbox"
-            value=""
-            class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            for="default-checkbox11"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >Default checkbox</label
+        <template v-for="todo in todos">
+          <li
+            v-if="todo.status === 'working'"
+            :key="todo.id"
+            class="text-base tracking-wide leading-5 my-2"
           >
-        </li>
-        <li class="text-base tracking-wide leading-5 my-2">
-          <input
-            id="default-checkbox22"
-            type="checkbox"
-            value=""
-            class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            for="default-checkbox22"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >Default checkbox</label
-          >
-        </li>
-        <li class="text-base tracking-wide leading-5 my-2">
-          <input
-            id="default-checkbox33"
-            type="checkbox"
-            value=""
-            class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            for="default-checkbox33"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >Default checkbox</label
-          >
-        </li>
-        <li class="text-base tracking-wide leading-5 my-2">
-          <input
-            id="default-checkbox44"
-            type="checkbox"
-            value=""
-            class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            for="default-checkbox44"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >Default checkbox</label
-          >
-        </li>
+            <input
+              :id="todo.id"
+              type="checkbox"
+              value=""
+              class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+              :for="todo.id"
+              class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >{{ todo.text }}</label
+            >
+          </li>
+        </template>
       </ul>
     </div>
     <div class="mb-5">
       <h3 class="text-lg">Done</h3>
       <ul>
-        <li class="text-base tracking-wide leading-5 my-2">
-          <input
-            id="default-checkbox111"
-            type="checkbox"
-            value=""
-            class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            for="default-checkbox111"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300 line-through"
-            >Default checkbox</label
+        <template v-for="todo in todos">
+          <li
+            class="text-base tracking-wide leading-5 my-2"
+            v-if="todo.status === 'done'"
+            :key="todo.id"
           >
-        </li>
-        <li class="text-base tracking-wide leading-5 my-2">
-          <input
-            id="default-checkbox222"
-            type="checkbox"
-            value=""
-            class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            for="default-checkbox222"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300 line-through"
-            >Default checkbox</label
-          >
-        </li>
-        <li class="text-base tracking-wide leading-5 my-2">
-          <input
-            id="default-checkbox333"
-            type="checkbox"
-            value=""
-            class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            for="default-checkbox333"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300 line-through"
-            >Default checkbox</label
-          >
-        </li>
-        <li class="text-base tracking-wide leading-5 my-2">
-          <input
-            id="default-checkbox444"
-            type="checkbox"
-            value=""
-            class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            for="default-checkbox444"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300 line-through"
-            >Default checkbox</label
-          >
-        </li>
+            <p
+              class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300 line-through ml-0"
+            >
+              {{ todo.text }}
+            </p>
+          </li>
+        </template>
       </ul>
     </div>
   </main>
--- todos/src/views/TodoView.vue
+++ todos/src/views/TodoView.vue
@@ -1,18 +1,52 @@
 <script setup lang="ts">
-import List from "../components/TodoView/List.vue"
-import AddTodo from "../components/TodoView/Addtodo.vue"
+import List from "../components/TodoView/List.vue";
+import AddTodo from "../components/TodoView/Addtodo.vue";
+import { ref } from "vue";
+
+let id = 0;
+
+const todos = ref([
+  {
+    id: id++,
+    text: "hello world",
+    status: "pending",
+  },
+  {
+    id: id++,
+    text: "Why are you for living",
+    status: "working",
+  },
+  {
+    id: id++,
+    text: "I love hip hop",
+    status: "done",
+  },
+]);
+
+const newTodo = ref("");
+
+function addTodo(todo: { value: string; }) {
+
+todos.value.push({
+    id: id++,
+    text: todo.value,
+    status: "pending",
+  });
+
+  newTodo.value = "";
+}
 
 </script>
 
 <template>
   <main class="flex flex-col w-full items-center">
     <header class="text-2xl">May 01 <span class="text-sm">2022</span></header>
+    <p>{{ newTodo }}</p>
     <div
       class="w-full bg-[#FDF6F0] flex flex-col items-start p-5 shadow-md rounded-md mt-10"
     >
-  <List />
+      <List :todos="todos" />
     </div>
-  <AddTodo />
-
+    <AddTodo @addTodo="addTodo" />
   </main>
 </template>
